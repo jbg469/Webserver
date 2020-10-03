cda5e63771bf40ae755bef09053e4db2dc502cdb
jbg469 <72175659+jbg469@users.noreply.github.com>
2020-10-03 17:29:55 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,34 +1,30 @@
-#import socket module
+#John Guevara
+#This is a simple Python socket web server
 from socket import *
-import sys # In order to terminate the program
-
-def main():
+import sys 
+def webServer():
     port=13331
     serverSocket = socket(AF_INET, SOCK_STREAM)
-
-    #Prepare a sever socket
     serverSocket.bind(("localhost", port))
-    serverSocket.listen(1)
+    serverSocket.listen(1)#ready
 
     while True:
-        #Establish the connection
         print("Ready to serve on port:", port)
         connectionSocket, addr = serverSocket.accept()
         try:
-            message = connectionSocket.recv(1024)
-            print("message:", message)
-            filename = message.split()[1] #HTTP path 
-            f = open(filename[1:])
-            outputdata =f.read()
+            mess = connectionSocket.recv(1024)
+            print("HTTP MESSAGE: ", mess)
+            file = mess.split()[1] #HTTP path 
+            buffer = open(file[1:])
+            output =buffer.read()
            #Send one HTTP header line into socket
             connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
             #Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
+            for j in range(0, len(output)):
+                connectionSocket.send(output[j].encode())
             connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
-            #Send response message for file not found (404)
             connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
             connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
             #Close client socket
@@ -38,4 +34,4 @@ def main():
     sys.exit()  # Terminate the program after sending the corresponding data
 
 if __name__ == "__main__":
-    main()
+    webServer()
